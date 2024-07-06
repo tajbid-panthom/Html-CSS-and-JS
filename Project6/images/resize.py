5330d291c746bfb30f3cdf04f9df5b9e57d03c52
tajbid-panthom <163171017+tajbid-panthom@users.noreply.github.com>
2024-07-06 14:18:41 +0600

    Delete Project6 directory
    Signed-off-by: tajbid-panthom <163171017+tajbid-panthom@users.noreply.github.com>

--- Project6/images/resize.py
+++
@@ -1,4 +0,0 @@
-from PIL import Image
-im=Image.open("D:\\Sigma Web Development\\CSS\\dropdown\\images\\user.jpg")
-reimg=im.resize((118,118))
-reimg.save("D:\\Sigma Web Development\\CSS\\dropdown\\images\\User.jpg")
